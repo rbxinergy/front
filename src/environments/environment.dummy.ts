a28ed62aed99e564c7935a00b008253ef1876e76
Armin Vera <armin.vera@xinergy.cl>
2024-06-14 12:55:52 -0400

    Agrega dummy env

---
+++ src/environments/environment.dummy.ts
@@ -0,0 +1,19 @@
+export const environment = {
+    production: false,
+    apiUrls: {
+      role: 'http://localhost:8080/api/v1/role',
+      contact: 'http://localhost:8080/api/v1/contact',
+      company: 'http://localhost:8080/api/v1/company',
+      domain: 'http://localhost:8080/api/v1/domain',
+      serviceCategory: 'http://localhost:8080/api/v1/service-category',
+      login: 'http://localhost:8080/api/v1/login',
+      client: 'http://localhost:8080/api/v1/client',
+      provider: 'http://localhost:8080/api/v1/provider',
+      report: 'http://localhost:8080/api/v1/report',
+      subdomain: 'http://localhost:8080/api/v1/subdomain',
+      domainCategory: 'http://localhost:8080/api/v1/domain-category',
+      groupCompany: 'http://localhost:8080/api/v1/group-company',
+      serviceCompany: 'http://localhost:8080/api/v1/service-company',
+      user: 'http://localhost:8080/api/v1/user'
+    }
+  };
