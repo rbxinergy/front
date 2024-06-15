02aa4a7fc5be59f690510a97b9404a51c0874da4
Armin Vera <armin.vera@xinergy.cl>
2024-06-14 21:54:50 -0400

    Agrega server url

--- src/environments/environment.dummy.ts
+++ src/environments/environment.dummy.ts
@@ -1,19 +1,20 @@
 export const environment = {
     production: false,
+    serverUrl: 'http://localhost:8080',
     apiUrls: {
-      role: 'http://localhost:8080/api/v1/role',
-      contact: 'http://localhost:8080/api/v1/contact',
-      company: 'http://localhost:8080/api/v1/company',
-      domain: 'http://localhost:8080/api/v1/domain',
-      serviceCategory: 'http://localhost:8080/api/v1/service-category',
-      login: 'http://localhost:8080/api/v1/login',
-      client: 'http://localhost:8080/api/v1/client',
-      provider: 'http://localhost:8080/api/v1/provider',
-      report: 'http://localhost:8080/api/v1/report',
-      subdomain: 'http://localhost:8080/api/v1/subdomain',
-      domainCategory: 'http://localhost:8080/api/v1/domain-category',
-      groupCompany: 'http://localhost:8080/api/v1/group-company',
-      serviceCompany: 'http://localhost:8080/api/v1/service-company',
-      user: 'http://localhost:8080/api/v1/user'
+      role: `/api/v1/role`,
+      contact: `/api/v1/contact`,
+      company: `/api/v1/company`,
+      domain: `/api/v1/domain`,
+      serviceCategory: `/api/v1/service-category`,
+      login: `/api/v1/login`,
+      client: `/api/v1/client`,
+      provider: `/api/v1/provider`,
+      report: `/api/v1/report`,
+      subdomain: `/api/v1/subdomain`,
+      domainCategory: `/api/v1/domain-category`,
+      groupCompany: `/api/v1/group-company`,
+      serviceCompany: `/api/v1/service-company`,
+      user: `/api/v1/user`
     }
   };
