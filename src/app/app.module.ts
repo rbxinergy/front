96b0ef21bfb333d82c7c2e8d3b247ed96a61f37d
Armin Vera <armin.vera@xinergy.cl>
2024-09-06 13:28:55 -0400

    Backoffice migrado a Angular 17, packages homologados pre unificación de aplicaciones.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,32 +1,27 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
-import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
-import { environment } from '../environments/environment';
-import { provideAuth, getAuth } from '@angular/fire/auth';
-import { SignUpComponent } from './components/sign-up/sign-up.component';
-import { AngularFireModule } from '@angular/fire/compat';
-import { HttpClientModule, HttpClient, HTTP_INTERCEPTORS } from '@angular/common/http';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatSlideToggleModule } from '@angular/material/slide-toggle';
-import {MatFormFieldModule} from '@angular/material/form-field';
-import {MatSelectModule} from '@angular/material/select';
-import {MatInputModule} from '@angular/material/input';
-import {MatIconModule} from '@angular/material/icon';
-import {MatListModule} from '@angular/material/list'; 
-import { FormsModule } from '@angular/forms';
-import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from '@angular/material/core';
-import { StepperModule } from './components/dashboard/stepper.module';
-import { ObjectToArrayPipe } from 'src/app/object-to-array.pipe';
-import { CommonModule } from '@angular/common';
-import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
-import { TranslateHttpLoader } from '@ngx-translate/http-loader';
-import { MatDialogModule } from '@angular/material/dialog';
-import { AuthInterceptor } from './shared/interceptors/auth.interceptor';
-import { ConfigsComponent } from './components/configs/configs.component';
-import { ServiceCategoryTableComponent } from './components/service-category-table/service-category-table.component';
-import { ContactModule } from './contact/contact.module';
+import { CommonModule } from "@angular/common";
+import { HttpClient, HttpClientModule, HTTP_INTERCEPTORS } from "@angular/common/http";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
+import { ErrorStateMatcher, ShowOnDirtyErrorStateMatcher } from "@angular/material/core";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { BrowserModule } from "@angular/platform-browser";
+import { BrowserAnimationsModule } from "@angular/platform-browser/animations";
+import { TranslateModule, TranslateLoader } from "@ngx-translate/core";
+import { TranslateHttpLoader } from "@ngx-translate/http-loader";
+import { AppRoutingModule } from "./app-routing.module";
+import { AppComponent } from "./app.component";
+import { StepperModule } from "./components/dashboard/stepper.module";
+import { SignUpComponent } from "./components/sign-up/sign-up.component";
+import { ContactModule } from "./contact/contact.module";
+import { ObjectToArrayPipe } from "./object-to-array.pipe";
+import { AuthInterceptor } from "./shared/interceptors/auth.interceptor";
+
 
 
 export function HttpLoaderFactory(http: HttpClient){
@@ -46,9 +41,6 @@ export function HttpLoaderFactory(http: HttpClient){
     AppRoutingModule,
     MatSlideToggleModule,
     MatFormFieldModule, MatInputModule, MatSelectModule,MatIconModule, MatListModule,
-    provideFirebaseApp(() => initializeApp(environment.firebase)),
-    provideAuth(() => getAuth()),
-    AngularFireModule.initializeApp(environment.firebase),
     HttpClientModule,
     TranslateModule.forRoot({
       loader: {
@@ -72,3 +64,11 @@ export function HttpLoaderFactory(http: HttpClient){
   ]
 })
 export class AppModule { }
+function provideAuth(arg0: () => any): any[] | import("@angular/core").Type<any> | import("@angular/core").ModuleWithProviders<{}> {
+  throw new Error('Function not implemented.');
+}
+
+function getAuth() {
+  throw new Error('Function not implemented.');
+}
+
